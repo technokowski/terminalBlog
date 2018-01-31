9ef6c069c743d64260be41105f132711350b7f82
technokowski <gabrielkavaloski@gmail.com>
2018-01-30 20:37:14 -0800

    list comp examples

--- appMongo.py
+++ appMongo.py
@@ -2,14 +2,22 @@ __author__ = "technokowski"
 
 import pymongo
 
+# On start, type mongod in one terminal, then mongo in another:
+# show dbs,
+
+# To kill the mongod process when restarting: ps ax | grep mongod
+# then: kill -9 mongod
+
 uri = "mongodb://127.0.0.1:27017"
 client = pymongo.MongoClient(uri)
 database = client['fullstack']
 collection = database['students']
 
-students = collection.find({})
+students = [person['mark'] for person in collection.find({}) if person['mark'] >= 99]
 
-for student in students:
-    print(student)
+#for student in students:
+#    print(student)
 
+
+print(students)
 
