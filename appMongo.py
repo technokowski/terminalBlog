5403def858f11d25f98720fe96d96448a6ceac0d
technokowski <gabrielkavaloski@gmail.com>
2018-01-30 22:11:11 -0800

    created database and post class

--- appMongo.py
+++ appMongo.py
@@ -1,23 +1,15 @@
+from database import Database
+import pymongo
+from models.post import Post
+
 __author__ = "technokowski"
 
-import pymongo
-
-# On start, type mongod in one terminal, then mongo in another:
-# show dbs,
-
-# To kill the mongod process when restarting: ps ax | grep mongod
-# then: kill -9 mongod
-
-uri = "mongodb://127.0.0.1:27017"
-client = pymongo.MongoClient(uri)
-database = client['fullstack']
-collection = database['students']
-
-students = [person['mark'] for person in collection.find({}) if person['mark'] >= 99]
-
-#for student in students:
-#    print(student)
 
 
-print(students)
+Database.initialize()
+
+myPost = Post('Movies', 'fartman', 'Yes he is!', 'Turd Furgeson', 'Jan 31 2018', 1)
+
+print(myPost.content)
+
 
---
+++ models/post.py
@@ -0,0 +1,41 @@
+from database import Database
+
+__author__ = "technokowski"
+
+'''
+This class initializes the Post object that will become a blog post. The json method
+is converting the self. to a json format, and the save_to_mongo is inserting it into
+the collections db using the insert method contained in Database.py. Pretty simple, actually.
+'''
+
+class Post(object):
+
+    def __init__(self, blog_id, title, content, author, date, id):
+        self.blog_id = blog_id
+        self.title = title
+        self.content = content
+        self.author = author
+        self.created_date = date
+        self.id = id
+
+    def save_to_mongo(self):
+        Database.insert(collection='posts',
+                        data=self.json())
+
+    def json(self):
+        return {
+            'id': self.id,
+            'blog_id': self.blog_id,
+            'content': self.content,
+            'author': self.author,
+            'title': self.title,
+            'created_date': self.created_date
+        }
+    @staticmethod
+    def from_mongo(id):
+        # Post.from_mongo('123')
+        return Database.find_one(collection='post', query={'id': id})
+
+    @staticmethod
+    def from_blog(id):
+        return [post for post in Database.find(collection='posts', query={'blog_id': id})]
